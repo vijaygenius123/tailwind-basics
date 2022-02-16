849246f2e637a7b231db6a111e58031729d7a3fe
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-02-16 19:32:57 +0000

    Created A Basic Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import Header from './Components/Header'
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <div className="container mx-auto">
+                <Header />
+                <h1 className="text-2xl text-blue-500">Hello World</h1>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/Header/constants.ts
@@ -0,0 +1,8 @@
+const navigation = [
+    {name: 'Dashboard', href: '#', current: true},
+    {name: 'Team', href: '#', current: false},
+    {name: 'Projects', href: '#', current: false},
+    {name: 'Calendar', href: '#', current: false},
+]
+
+export {navigation}
