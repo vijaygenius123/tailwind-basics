eab71b5ebe0d5e36a5232a9d1c513ab667e275fe
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-02-24 21:10:34 +0000

    Basic Setup Without Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,56 @@ import Header from './Components/Header'
 
 function App() {
     return (
-        <>
-            <div className="container mx-auto">
-                <Header />
-                <h1 className="text-2xl text-blue-500">Hello World</h1>
-            </div>
-        </>
+       <div>
+           <div>
+               <nav>
+                   <div>
+                       <h1>
+                           <a href="">Tailwind</a>
+                       </h1>
+                   </div>
+                   <ul>
+                       <li>
+                           <a href="">Home</a>
+                       </li>
+                       <li>
+                           <a href="">About</a>
+                       </li>
+                       <li>
+                           <a href="">Contact</a>
+                       </li>
+                   </ul>
+               </nav>
+           </div>
+
+           <main>
+               <div>
+                   <a href="">Log In</a>
+                   <a href="">Sign up</a>
+               </div>
+
+
+               <header>
+                   <h2>Recipes</h2>
+                   <h3>For Tailwind</h3>
+               </header>
+
+               <div>
+                   <h4>Latest Recipes</h4>
+
+                   <div>
+                       <div>
+                           <img src="" alt=""/>
+                           <div>
+                               <span>Simple Web Site</span>
+                               <span>By Tailwind</span>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+
+           </main>
+       </div>
     );
 }
 
